bd0ecf13f8fce1e13a47d907e248e4349da05c03
tom-aglow <tom@ohhhh.me>
2017-09-18 14:01:18 -0700

    make filter for parties

--- public/js/compare.js
+++ public/js/compare.js
@@ -2,4 +2,20 @@ $(function () {
 	
 	//	initialize multiselect element
 	$('#select-policies').multiselect();
+	
+	$('.filters__toggle').click(function () {
+		var $this = $(this);
+		var party = $this.data('party');
+
+		//	toggle active class for party filter element
+		$this.toggleClass('active');
+		$('.policies [data-party="' + party + '"]').toggleClass('hidden');
+
+		//	calculate how many columns should be displayed
+		var colNum = $('.filters__toggle.active').length;
+		var gridTemplateColumns = (colNum > 0) ? '120px repeat(' + colNum + ', 1fr)' : 'none';
+		$('.policies').css('grid-template-columns', gridTemplateColumns);
+
+		console.log($this.data('party'));
+	});
 });
