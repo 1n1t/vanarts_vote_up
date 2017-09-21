ea5454803a572a2c5a3e23c6e446a1c1cc88436f
Nina Louw <ninalouw@mac.com>
2017-09-21 13:56:33 -0700

    trying to access votes values, removed unnecessary colours

--- public/js/doughnutchart.js
+++ public/js/doughnutchart.js
@@ -1,19 +1,31 @@
 
-// const votesArray = votes.map((vote) => {
-// 	return vote.value;
-// });
+const partiesArray = Object.keys(votes);
+console.log('Parties', partiesArray);
 
-// const partyNamesArray = votes.map((vote) => {
-// 	return vote.name;
+const votesArray = [];
+for (var key in votes) {
+	if (votes.hasOwnProperty(key)) {
+		console.log('key', key);
+		console.log('votes[key]', votes[key]);
+		votesArray.push(votes[key].length);
+		console.log('array', votesArray);
+	}
+}
+// const votesArray = [];
+// Object.keys(votes).forEach(function (key) {
+// 	console.log(votes[key].length);
+// 	votesArray.push(votes[key].length);
+// 	console.log(votesArray);
 // });
-// const votesArray = [votes[]]
-const votesArray = [1, 5, 7, 3];
+// const votesArray = Object.values(votes);
+// console.log(votesArray);
+
 
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
 	type: 'doughnut',
 	data : {
-		labels: ["Conservative", "Liberal", "Green", "New Democratic"],
+		labels: partiesArray,
 		datasets: [{
 				label: "Number of Votes",
 				data: votesArray,
@@ -21,31 +33,13 @@ var myChart = new Chart(ctx, {
 					"rgb(0, 102, 255)",
 					"#F7464A",
 					"#00BFA5",
-					"rgb(255, 102, 0)",
-					"rgb(246, 138, 50)",
-					"#00B8D4",
-					"rgb(73, 101, 185)",
-					"rgb(161, 87, 196)",
-					"#C51162",
-					"rgb(158, 4, 18)",
-					"rgb(149, 59, 9)",
-					"rgb(75, 227, 175)",
-					"rgb(249, 123, 7)"
+					"rgb(255, 102, 0)"
 				],
 				hoverBackgroundColor: [
 					"rgb(0, 102, 255)",
 					"#FF5A5E",
 					"#5AD3D1",
-					"rgb(255, 153, 51)",
-					"rgb(242, 118, 57)",
-					"rgb(97, 190, 214)",
-					"rgb(105, 123, 213)",
-					"rgb(184, 73, 212)",
-					"rgb(195, 63, 119)",
-					"rgb(171, 44, 48)",
-					"rgb(176, 94, 48)",
-					"rgb(134, 244, 178)",
-					"rgb(246, 143, 85)"
+					"rgb(255, 153, 51)"
 				]
 		}]
 	},
