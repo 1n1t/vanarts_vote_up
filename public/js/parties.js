651c1f4974eff4f2ce002fa4a3d067030ac79001
Nina Louw <ninalouw@mac.com>
2017-09-21 22:56:31 -0700

    update parties.json, append data to elems using jquery in parties.js

--- public/js/parties.js
+++ public/js/parties.js
@@ -2,4 +2,26 @@
 //	*** POPULATING PARTIES PAGE ***
 // ----------------------------------
 	
-var leadershipInfo = parseJSON('/db/policies.json');
+//parse the json file
+var parties = parseJSON('/db/parties.json');
+//jquery function
+$(function(){
+	//populate the p tag with leadership data
+	for(const party in parties){
+		for(const partyObjKey in parties[party]){
+			console.log(party ,':', partyObjKey, );
+			console.log(parties[party][partyObjKey]);
+			//1. append leadership info to leadership p tag
+			$('.leadership-para[data-leadership="' + party + '"][data-info="' + partyObjKey + '"]')
+				.text(parties[party][partyObjKey]);
+
+			//2. append policy info to policy p tag
+			$('.policies-para[data-leadership="' + party + '"][data-info="' + partyObjKey + '"]')
+				.text(parties[party][partyObjKey]);
+
+			//3. append history info to history p tag
+			$('.history-para[data-leadership="' + party + '"][data-info="' + partyObjKey + '"]')
+				.text(parties[party][partyObjKey]);
+		}
+	}
+});
