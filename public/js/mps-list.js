2841938d1baf0de766e1d4704406f09a908cf454
tom-aglow <tom@ohhhh.me>
2017-09-24 15:18:13 -0700

    create DOM-elements with MPs information for each province and party using data from JSON-file // tweak css-styles

--- public/js/mps-list.js
+++ public/js/mps-list.js
@@ -25,14 +25,50 @@ $(function () {
 		var provinceEl = $("<article class='province'></article>");
 		var provinceNameEl = $("<h1></h1>").text(province);
 
+		//	add province name element to the province section
 		provinceEl.append(provinceNameEl);
 
-		parties.forEach(function (party) {
-			var partyEl = $("<article class='party'></article>");
-			var partyNameEl = $("<h3></h3>").text(party);
+		//	get the object with all MPs in this province
+		var mpsObj = {};
 
-			partyEl.append(partyNameEl);
-			provinceEl.append(partyEl);
+		mps["MemberOfParliament"].forEach(function (mp) {
+			if (mp.ConstituencyProvinceTerritoryName === province) {
+
+				if(!mpsObj.hasOwnProperty(mp.CaucusShortName)){
+					mpsObj[mp.CaucusShortName] = [];
+				}
+
+				mpsObj[mp.CaucusShortName].push({
+					fullName: mp.PersonOfficialFirstName + ' ' + mp.PersonOfficialLastName,
+					district: mp.ConstituencyName
+				});
+			}
+		});
+		
+		parties.forEach(function (party) {
+			
+			//	check if there is at least one MP from the party in that province
+			if (mpsObj.hasOwnProperty(party)) {
+				
+				//	create party sections with name of the party
+				var partyEl = $("<article class='party'></article>");
+				var partyNameEl = $("<h3></h3>").text(party);
+
+				partyEl.append(partyNameEl);
+
+				var mpContainerEl = $("<div class='mp-container'></div>");
+				
+				mpsObj[party].forEach(function (mp) {
+					var mpEl = $("<div class='mp'></div>");
+					mpEl.append($("<p class='mp-name'></p>").text(mp.fullName));
+					mpEl.append($("<p class='mp-district'></p>").text(mp.district));
+					
+					mpContainerEl.append(mpEl);
+				});
+
+				partyEl.append(mpContainerEl);
+				provinceEl.append(partyEl);
+			}
 		});
 		
 		container.append(provinceEl);
