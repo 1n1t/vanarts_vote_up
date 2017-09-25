7d6a8f9154d41e3251a15cdcbb9c6968796dcc73
tom-aglow <tom@ohhhh.me>
2017-09-25 14:21:08 -0700

    add back-to-top button and handler for click events on it

--- public/js/compare.js
+++ public/js/compare.js
@@ -291,6 +291,60 @@ $(function () {
 			$('.policies [data-party="' + parties[i] + '"]').css('order', i);
 		}
 	}
+
+	// ------------------------------
+	//	*** BACK TO TOP SCROLLING ***
+	// ------------------------------
+	
+	$('.back-to-top').click(function () {
+		smoothScrollToTarget($('body'));
+	});
+
+	$(window).scroll(function () {
+		//  scrolling function
+		var $this = $(this);
+		var wScroll = $this.scrollTop();
+		
+		if (wScroll > 500) {
+			$('.back-to-top').removeClass('hidden');
+		} else {
+			$('.back-to-top').addClass('hidden');
+		}
+	});
+
+	function smoothScrollToTarget(target) {
+		let startY = $(window).scrollTop();
+		let adj = 0;
+		let stopY = target.offset().top - adj;
+		let distance = stopY > startY ? stopY - startY : startY - stopY;
+
+		if (distance < 100) {
+			scrollTo(0, stopY);
+			return;
+		}
+
+		let speed = Math.round(distance / 100);
+
+		if (speed >= 20) speed = 20;
+		let step = Math.round(distance / 25);
+		let leapY = stopY > startY ? startY + step : startY - step;
+		let timer = 0;
+
+
+		if (stopY > startY) {
+			for ( let i=startY; i<stopY; i+=step ) {
+				setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+				leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+			}
+			return;
+		}
+		for ( let i=startY; i>stopY; i-=step ) {
+			setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+			leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+		}
+
+		return false;
+	}
 });
 
 
