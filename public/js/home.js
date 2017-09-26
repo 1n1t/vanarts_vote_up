551bb2195b9d7f24117461887ab54eec0cf3c770
sandyh92 <gumii711@gmail.com>
2017-09-26 16:27:44 -0700

    revisions to the home page

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	function randomizeVideo(){
-		var videosArray = ["D59v74k5flU", "vC510ZXaSXQ", "wcMq3LXp674", "dt__kig8PVU"];
+		var videosArray = ["GADdPGkVuss", "wD5FAzvltpI", "yLshbuVJH5Q", "uVhPlcy7GDs"];
 		//Pick a random index number from the array
 		var randomIndex = videosArray[Math.floor(Math.random() * videosArray.length)];
 		var videoUrl = "http://www.youtube.com/embed/" + randomIndex + "?autoplay=0&amp;frameborder=0&amp;controls=0&amp;modestbranding=1&amp;showinfo=0&amp;rel=0&amp;disablekb=1&amp;start=0&amp;iv_load_policy=3";
